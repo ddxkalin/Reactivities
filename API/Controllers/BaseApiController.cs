32f314209b43ec6ba5a16c0229f9f1b0fb1d1cc3
Kalin Stoev <kstoev9316@gmail.com>
2022-10-06 20:03:24 +0300

    Added BaseController and ActivitiesController

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+namespace API.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
