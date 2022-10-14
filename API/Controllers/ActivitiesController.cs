46dda83859887a93663eb2779577d42e66df5e24
Kalin Stoev <kstoev9316@gmail.com>
2022-10-14 17:53:30 +0300

    Mediator and CQRS added with List and Details

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,30 +1,23 @@
-using Domain;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
-
 namespace API.Controllers
 {
+    using Application.Activities;
+    using Domain;
+    using Microsoft.AspNetCore.Mvc;
 
     public class ActivitiesController : BaseApiController
-    {
-        private readonly DataContext _context;
-
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
-
+    {    
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query{
+                Id = id
+            });
         }
     }
 }
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,11 +1,20 @@
 namespace API.Controllers
 {
+    using MediatR;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.DependencyInjection;
 
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices
+            .GetService<IMediator>();
 
+        public BaseApiController()
+        {
+
+        }
     }
 }
