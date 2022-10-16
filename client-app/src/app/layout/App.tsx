6f4d2475406081984955f702f78436b45c03821e
Kalin Stoev <kstoev9316@gmail.com>
2022-10-16 13:43:07 +0300

    NavBar Added

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import './styles.css';
 import  axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -14,16 +15,18 @@ function App() {
   }, [])
 
   return (
-    <div>
-      <Header as='h2' icon='users' content='Reactivities' />
-      <List>
-        {activities.map(activity => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-      </List>
-    </div>
+    <>
+      <NavBar/>
+        <Container style={{ marginTop: '7em'}}>
+          <List>
+            {activities.map(activity => (
+                <List.Item key={activity.id}>
+                  {activity.title}
+                </List.Item>
+              ))}
+          </List>
+        </Container>
+      </>
   );
 }
 
