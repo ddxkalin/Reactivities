6a9bd003069f759dbd9f05274ddb211005b1ddad
Kalin <kstoev9316@gmail.com>
2022-12-02 17:31:42 +0200

    Edit Profile Added

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -1,20 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Application.Profiles;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-
 namespace API.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
     public class ProfilesController : BaseApiController
     {
         [HttpGet("{username}")]
         public async Task<IActionResult> GetProfile(string username)
         {
-            return HandleResult(await Mediator.Send(new Details.Query{Username = username}));
+            return HandleResult(await Mediator.Send(new Application.Profiles.Details.Query { Username = username }));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Edit(Application.Profiles.Edit.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
         }
     }
 }
