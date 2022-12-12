468c7a5320e8eeb25cd34f2d6a555ba9e817b041
Kalin <kstoev9316@gmail.com>
2022-12-12 15:17:46 +0200

    Pagination API

---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,22 @@
+namespace API.Extensions
+{
+    using System.Text.Json;
+
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse response, int currentPage,
+            int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationheader = new 
+            {
+                currentPage,
+                itemsPerPage,
+                totalItems,
+                totalPages
+            };
+            
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationheader));
+            response.Headers.Add("Accesss-Control-Expose-Headers", "Pagination");
+        }
+    }
+}
---
+++ Application/Activities/ActivityParams.cs
@@ -0,0 +1,13 @@
+namespace Application.Activities
+{
+    using Application.Core;
+
+    public class ActivityParams : PagingParams
+    {
+        public bool IsGoing { get; set; }
+
+        public bool IsHost { get; set; }
+        
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
+    }
+}
