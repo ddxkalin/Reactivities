b6314854d6e7f8bd0b3aec95e8db2eda3ad0383b
Kalin <kstoev9316@gmail.com>
2022-12-14 20:30:24 +0200

    Login Form fix

--- client-app/src/features/users/LoginForm.tsx
+++ client-app/src/features/users/LoginForm.tsx
@@ -15,7 +15,7 @@ export default observer(function LoginForm() {
             {({handleSubmit, isSubmitting, errors}) => (
                 <Form className="ui form" onSubmit={handleSubmit} autoComplete='off'>
                     <Header as="h2" content='Login to Reactivities' color="teal" textAlign="center"/>
-                    <MyTextInput name="email" placeholder="Email" />x
+                    <MyTextInput name="email" placeholder="Email" />
                     <MyTextInput name="password" placeholder="Password" type='password'/>
                     <ErrorMessage 
                         name='error' render={() => 
